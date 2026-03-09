ad9e67f125133af3e86e9f09bf19fc6dea6eb3b6
Jose Antonio <joseantoniomdl95@gmail.com>
2026-03-09 13:43:49 +0100

    fix railway db connection

--- config/database.php
+++ config/database.php
@@ -10,7 +10,7 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
@@ -18,8 +18,7 @@ try {
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
     http_response_code(500);
-   // echo json_encode(['error' => 'Error de conexión a la base de datos']);
-   echo json_encode([
+    echo json_encode([
         'error' => 'Error de conexión a la base de datos',
         'detalle' => $e->getMessage()
     ]);
