5f829a41b6aee23e733d4c6cc5d9ca932b8679eb
Jose Antonio <joseantoniomdl95@gmail.com>
2026-03-09 14:30:05 +0100

    debug de time y zona horaroia madrid

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Europe/Madrid');
 header("Access-Control-Allow-Origin: https://inaltera-frontend.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
--- src/Controllers/InvoiceController.php
+++ src/Controllers/InvoiceController.php
@@ -147,10 +147,7 @@ class InvoiceController {
                 throw new Exception('Fecha inválida');
             }
             if (strtotime($data['fecha_emision']) > time()) {
-                error_log("fecha_emision: " . $data['fecha_emision']);
-                error_log("strtotime: " . strtotime($data['fecha_emision']));
-                error_log("time(): " . time());
-                throw new Exception("La fecha no puede ser futura");
+                throw new Exception("La fecha no puede ser futura" . " fecha_emision: " . $data['fecha_emision']. " strtotime: " . strtotime($data['fecha_emision'] . "time(): " . time()) );
             }
 
             if (!isset($data['lines']) || !is_array($data['lines']) || count($data['lines']) === 0) {
